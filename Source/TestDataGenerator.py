981bd82a1dad5fb5b27765ddf3458a980c7215e2
K <Akarfire@gmail.com>
2026-03-12 19:02:17 +0300

    [DEV] Test data generation

--- Source/TestDataGenerator.py
+++ Source/TestDataGenerator.py
@@ -1,3 +1,26 @@
+import numpy as np
 
-with open("Data/TestData.txt", 'w') as f:
-    f.write("TestData")
+def generate_test_data(num_entries : int):
+    
+    with open("Data/TestData.txt", 'w') as f:
+        
+        rand_h1 = np.random.rand(num_entries)
+        rand_h2 = np.random.rand(num_entries)
+        
+        rand_s1 = np.random.rand(num_entries)
+        rand_s2 = np.random.rand(num_entries)
+        
+        rand_a1 = np.random.rand(num_entries)
+        rand_a2 = np.random.rand(num_entries)
+        
+        rand_b1 = np.random.rand(num_entries)
+        rand_b2 = np.random.rand(num_entries)
+        
+        f.write("# h1 | h2 | s1 | s2 | a1 | a2 | b1 | b2\n")
+        for i in range(num_entries):
+            line = f"{rand_h1[i]} | {rand_h2[i]} | {rand_s1[i]} | {rand_s2[i]} | {rand_a1[i]} | {rand_a2[i]} | {rand_b1[i]} | {rand_b2[i]}\n"
+            f.write(line)
+        
+        
+if __name__  == "__main__":
+    generate_test_data(1000)
