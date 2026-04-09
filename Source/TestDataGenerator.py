ce21b6f78d1df8907e7a467b13f9381cf8744d12
unknown <the_h1nk@mail.ru>
2026-04-09 21:45:07 +0300

    [DEV] Add z1_0, z2_0 in test and learning data

--- Source/TestDataGenerator.py
+++ Source/TestDataGenerator.py
@@ -15,10 +15,15 @@ def generate_test_data(num_entries : int):
         
         rand_b1 = np.random.rand(num_entries)
         rand_b2 = np.random.rand(num_entries)
+
+        rand_z1_0 = np.random.rand(num_entries)
+        rand_z2_0 = np.random.rand(num_entries)
+
+
         
-        f.write("# h1 | h2 | s1 | s2 | a1 | a2 | b1 | b2\n")
+        f.write("# h1 | h2 | s1 | s2 | a1 | a2 | b1 | b2 | z1_0 | z2_0\n")
         for i in range(num_entries):
-            line = f"{rand_h1[i]} | {rand_h2[i]} | {rand_s1[i]} | {rand_s2[i]} | {rand_a1[i]} | {rand_a2[i]} | {rand_b1[i]} | {rand_b2[i]}\n"
+            line = f"{rand_h1[i]} | {rand_h2[i]} | {rand_s1[i]} | {rand_s2[i]} | {rand_a1[i]} | {rand_a2[i]} | {rand_b1[i]} | {rand_b2[i]} | {rand_z1_0[i]} | {rand_z2_0[i]}\n"
             f.write(line)
         
         
