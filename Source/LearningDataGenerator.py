b7d7483880fa679aef2c3274828a644df922c652
K <Akarfire@gmail.com>
2026-05-14 17:36:14 +0300

    [DEV] Learning data generator refactor

--- Source/LearningDataGenerator.py
+++ Source/LearningDataGenerator.py
@@ -1,6 +1,7 @@
-from TestDataGenerator import generate_test_data
-from DifferentialSolver import solve_population_equation, Parameters
+from Types import Coefficients, Parameters
 
+from TestDataGenerator import generate_coefficients
+from DifferentialSolver import solve_population_equation
 
 def generate_learning_data(num_entries):
     
@@ -9,36 +10,30 @@ def generate_learning_data(num_entries):
     p = 0.3
     q = 0.3
     
+    # Output
+    res : list[tuple[bool, Coefficients]] = []
+    
+    while (len(res) < num_entries):
+        coefficients = generate_coefficients(num_entries)
+        for c in coefficients:
+            try:
+                params = Parameters.from_coefficients(c, r, p, q)
+                res_1, res_2 = solve_population_equation(params)
+                
+                res.append((0 if res_1 > res_2 else 1, c))
+            except: pass
+            
+    return res
+               
+        
+def write_learning_data(data : list[tuple[bool, Coefficients]]):
     with open("Data/LearningData2.txt", 'w') as output:
         output.write("# Winner ID (0 or 1) | h1 | h2 | s1 | s2 | a1 | a2 | b1 | b2 | z1_0 | z2_0 \n")
         
-        generate_test_data(num_entries * 2)
-        test_data_file = open("Data/TestData.txt")
-        test_data_lines = test_data_file.readlines()
-        
-        i = 0
-        num_good_entries = 0
-        while(num_good_entries < num_entries):
-            i += 1
-               
-            line = test_data_lines[i] 
-            if line.startswith('#'): continue
-            
-            # h1 | h2 | s1 | s2 | a1 | a2 | b1 | b2 | z1_0 | z2_0
-            h1, h2, s1, s2, a1, a2, b1, b2, z1_0, z2_0 = [float(i) for i in line.split('|')]
-            
-            params = Parameters(h1, h2, s1, s2, a1, a2, b1, b2, z1_0, z2_0, r, p, q)
-            try:
-                res_1, res_2 = solve_population_equation(params)
-                num_good_entries += 1
-                output.write(f"{0 if res_1 > res_2 else 1} | {h1} | {h2} | {s1} | {s2} | {a1} | {a2} | {b1} | {b2} | {z1_0} | {z2_0}\n")
-                
-            except:
-                pass
-            
-        test_data_file.close()
-        
+        for d in data:
+            output.write(f"{d[0]} | {d[1].h1} | {d[1].h2} | {d[1].s1} | {d[1].s2} | {d[1].a1} | {d[1].a2} | {d[1].b1} | {d[1].b2} | {d[1].z1_0} | {d[1].z2_0}\n")
         
 # Script
 if __name__  == "__main__":
-    generate_learning_data(5000)
+    data = generate_learning_data(5000)
+    write_learning_data(data)
